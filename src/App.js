b39ba028f70c60e442aa0de1640f1c01592d2081
Tanay Bagayatkar <42608387+TanayBagayatkar@users.noreply.github.com>
2021-07-03 17:49:12 +0530

    changes images folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-
-import './App.css';
-import Navbar from './component/Navbar';
-import Home from './component/Home';
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import Grid from "@material-ui/core/Grid";
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/> */}
-      <Home/>
-      
+      <Navbar />
+      {/* columns={{ xs: 12, sm: 8, md: 12 }} */}
+      <Grid container >
+        {/* <Grid item xs={12} md={4} >
+        <Home />
+        </Grid> */}
+        <Grid item xs={12} md={12} >
+        <Home />
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,56 +1,64 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
-import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
-
-// import {SkipNextRoundedIcon} from '@material-ui/icons';
-import { AccessAlarm, ThreeDRotation, SkipNext, SkipPreviousRounded } from '@material-ui/icons';
-// import SkipPreviousRoundedIcon from '@material-ui/icons/SkipPreviousRounded';
+import { SkipNext, SkipPreviousRounded } from '@material-ui/icons';
+// import '../images/mta.jpg';
+import mta from '../images/mta.jpg';
+import google from '../images/google.jpg';
+import reactbasic  from  '../images/reactbasic.png';
+import courserapython  from  '../images/coursera-python.jpg';
+import courserabootstrap from '../images/coursera-bootstrap.jpg';
+import rpa from '../images/rpa.jpg';
+import cloud from '../images/cloud.jpg';
+import problemsolvingbasic from '../images/problemsolvingbasic.png';
+import abhigyaan from '../images/abhigyaan.jpg';
+import ignium from '../images/ignium.jpg';
+import tempdiploma from '../images/tempdiploma.jpg';
 
 let Certificates = [
   {
     id: 1,
-    name: "mta.jpg",
+    name: mta,
   },
   {
     id: 2,
-    name: "google.jpg",
+    name: google,
   },
   {
     id: 3,
-    name: "reactbasic.png",
+    name: reactbasic,
   },
   {
     id: 4,
-    name: "coursera-python.jpg",
+    name: courserapython,
   },
   {
     id: 5,
-    name: "rpa.jpg",
+    name: rpa,
   },
   {
     id: 6,
-    name: "coursera-bootstrap.jpg",
+    name: courserabootstrap,
   },
   {
     id: 7,
-    name: "cloud.jpg",
+    name: cloud,
   },
   {
     id: 8,
-    name: "problemsolvingbasic.png",
+    name: problemsolvingbasic,
   },
   {
     id: 9,
-    name: "abhigyaan.jpg",
+    name: abhigyaan,
   },
   {
     id: 10,
-    name: "ignium.jpg",
+    name: ignium,
   },
   {
     id: 11,
-    name: "tempdiploma.jpg",
+    name: tempdiploma,
   },
 ];
 
@@ -66,22 +74,24 @@ function Home(props) {
   const prev = () => {
     let temp = id;
     setId(--temp);
-    console.log(id);
+    // console.log(id);
     setImageName(position.name);
   };
   const next = () => {
     let temp = id;
     setId(++temp);
-    console.log(id);
+    // console.log(id,position.name);
     setImageName(position.name);
   };
   const getData = () => {
     // let temp = id;
-    let source = `/images/${position.name}`;
+    let source = `${position.name}`;
+    // let source = require(`../images/mta.jpg`);
+    // src={require(`../images/${position.name}`)}
     return (
       <div className="body-certi">
         <Paper elevation={10}>
-          <img className="certificate" src={source} alt="certificate" />
+          <img className="certificate" src={position.name}  alt="certificate" />
         </Paper>
       </div>
     );
@@ -95,7 +105,7 @@ function Home(props) {
         }}
         disabled={id === 0}
       >
-        Prev <SkipPreviousRounded/>
+        <SkipPreviousRounded/>
       </Button>
       <Button
         onClick={() => {
@@ -103,7 +113,7 @@ function Home(props) {
         }}
         disabled={id === Certificates.length - 1}
       >
-        Next <SkipNext/>
+        <SkipNext/>
       </Button>
       <Button onClick={() => start()} disabled={id===0}>Goto Start</Button>
       {/* <h2>Certificate No. {id + 1}</h2> */}
